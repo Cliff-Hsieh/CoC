b91d06e1f1fd7b5d5990ebd5373a80770990a03d
root <root@ip-172-31-47-32.us-west-2.compute.internal>
2017-08-06 18:19:00 +0000

    feat: first version of fullpage js

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!doctype HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="https://alvarotrigo.com/fullPage/jquery.fullpage.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://alvarotrigo.com/fullPage/jquery.fullPage.min.js"></script> 
+</head>
+<body>
+<div id="fullpage">
+	<div class="section"><img src="https://s-media-cache-ak0.pinimg.com/originals/f0/b7/b9/f0b7b9646b0dac43b7203e8f353b54a5.jpg" /></div>
+	<div class="section"><img src="https://downloads.andyroid.net/website/v2//wp-content/uploads/2014/12/clash.png"></div>
+	<div class="section"><img src="http://loyalknights.com/wp-content/uploads/2014/10/CoC_titlescreen_2013.jpg"></div>
+	<div class="section"><img src="https://lh3.googleusercontent.com/LWO63qPS4lTHnAVPzHeTaefzZia5r5uodW6xHiifi7DBU6MqvoykF-tjXgNcQ499JA=h310" /></div>
+</div>
+</body>
+<script>
+$(document).ready(function() {
+  $('#fullpage').fullpage();
+});
+</script>
+</html>
